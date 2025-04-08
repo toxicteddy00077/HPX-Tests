43b361563f81e0859b201501b71b2ec34d5779dd
Aryamaan Singh <71913204+toxicteddy00077@users.noreply.github.com>
2025-04-08 16:33:34 +0530

    Update MatMul.cpp

--- HPX_MatMul/MatMul.cpp
+++ HPX_MatMul/MatMul.cpp
@@ -14,12 +14,12 @@ void tiled_matmul(std::vector<float>& A, std::vector<float>& B, std::vector<floa
     std::size_t blks=N/blk_size;
     // parallel loop over each individual block
     hpx::experimental::for_loop(hpx::execution::par, 0, blks, [&](std::size_t bi) {
-        for (auto bj=0; bj<blks; bj++)
+        for (auto bj=0;bj<blks; bj++)
         {
             for (auto bk=0;bk<blks; bk++)
             {
                 // individual block computation
-                for (auto i=bi * blk_size; i<(bi+1)*blk_size; i++)
+                for (auto i=bi*blk_size; i<(bi+1)*blk_size; i++)
                 {
                     for (auto j=bj*blk_size; j<(bj+1)*blk_size; j++)
                     {
@@ -34,20 +34,20 @@ void tiled_matmul(std::vector<float>& A, std::vector<float>& B, std::vector<floa
 }
 
 int main(){
-    std::size_t N = 1024; 
-    std::size_t blk_size = 32;
+    std::size_t N=1024; 
+    std::size_t blk_size=32;
 
     // Matrix init
     std::vector<float> A(N*N, 1.0); 
     std::vector<float> B(N*N, 2.0); 
     std::vector<float> C(N*N, 0.0); 
 
-    high_resolution_clock::time_point start = high_resolution_clock::now();
+    high_resolution_clock::time_point start=high_resolution_clock::now();
 
     // Perform tiled matrix multiplication
     tiled_matmul(A, B, C, N, blk_size);
 
-    high_resolution_clock::time_point end = high_resolution_clock::now();
+    high_resolution_clock::time_point end=high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(end - start).count();
     std::cout << "Time taken for tiled matrix multiplication: " << duration << " ms" << std::endl;
     
